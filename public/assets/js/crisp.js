3036b36b471502914a104e36aefcf8e0c39c280a
cjrussell23 <cj.russell2396@gmail.com>
2023-11-15 10:08:55 -0700

    Update crisp.js

--- public/assets/js/crisp.js
+++ public/assets/js/crisp.js
@@ -7,3 +7,9 @@ window.CRISP_WEBSITE_ID = "e044823b-9247-4d4c-a53d-79721b8b91d3";
   s.async = 1;
   d.getElementsByTagName("head")[0].appendChild(s);
 })();
+
+$crisp.push(["on", "message:sent", updateCrispSegments]);
+
+function updateCrispSegments() {
+  $crisp.push(["set", "session:segments", [["portal"]]]);
+}
